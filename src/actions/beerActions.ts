713257abd0723ed27dfae803c32e2ee45f56bcbb
Jacob Butler <jacobbutler@JacobButlersMBP.hsd1.ca.comcast.net>
2019-02-18 20:26:04 -0800

    add beer actions

---
+++ src/actions/beerActions.ts
@@ -0,0 +1,18 @@
+export const RECEIVE_BEERS = 'RECEIVE_BEERS';
+export const RECEIVE_BEER = 'RECEIVE_BEER';
+export const REMOVE_BEER = 'REMOVE_BEER';
+
+export const receiveBeers = (beers: any) => ({
+  type: RECEIVE_BEERS,
+  beers
+})
+
+export const receiveBeer = (beer: any) => ({
+  type: RECEIVE_BEER,
+  beer
+})
+
+export const removeBeer = (beer: any) => ({
+  type: REMOVE_BEER,
+  beer
+})
