c3a6476c39d34b1bac12a106904dd987d6955d26
Jacob Butler <jacobbutler@JacobButlersMBP.hsd1.ca.comcast.net>
2019-02-19 18:25:51 -0800

    add thunk and logger to middlewares

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,19 +1,13 @@
-import { createStore, applyMiddleware, Store } from 'redux';
-// import thunk from 'redux-thunk';
-// import logger from 'redux-logger';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 import rootReducer from '../reducers/rootReducer';
-
-// const middlewares = [ thunk ];
-
-// if (process.env.NODE_ENV !== 'production') {
-//   middlewares.push(logger);
-// }
 
 const configureStore = (preloadedState = {}) => 
   createStore(
     rootReducer,
     preloadedState,
-    // applyMiddleware(thunk)
+    applyMiddleware(thunk, logger)
   )
 ;
 
